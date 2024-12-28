1b109b536ba7e67e9b0ab9d791eb6c943d482fce
Kıvanç Onat Türker <kivo0326@gmail.com>
2024-12-28 13:39:30 +0300

    Final Version

--- Project/src/TranspositionEntry.java
+++ Project/src/TranspositionEntry.java
@@ -1,10 +1,11 @@
 public class TranspositionEntry 
 {
-    int value; // The evaluation of the state
-    int depth; // The depth at which this value was computed
-    boolean isExact; // Whether this value is exact (not a bound)
+    int value; 
+    int depth;
+    boolean isExact;
 
-    public TranspositionEntry(int value, int depth, boolean isExact) {
+    public TranspositionEntry(int value, int depth, boolean isExact) 
+    {
         this.value = value;
         this.depth = depth;
         this.isExact = isExact;
